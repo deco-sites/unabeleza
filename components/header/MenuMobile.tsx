968c9215d24bbae1bf5aa9b317da74bca5f826cb
georgetonietti <georgetonietti@gmail.com>
2024-10-16 17:20:13 -0300

    feat: drawer wip

---
+++ components/header/MenuMobile.tsx
@@ -0,0 +1,87 @@
+import Icon from "../../components/ui/Icon.tsx";
+import type { SiteNavigationElement } from "apps/commerce/types.ts";
+import Drawer from "../ui/Drawer.tsx";
+import { type LoadingFallbackProps } from "@deco/deco";
+
+export const LoadingFallback = (props: LoadingFallbackProps<MenuItemProps>) => (
+    // deno-lint-ignore no-explicit-any
+    <MenuItem {...props as any} loading="lazy" />
+);
+
+export interface Props {
+    navItems?: SiteNavigationElement[];
+}
+
+export interface MenuItemProps {
+    item: SiteNavigationElement,
+    loading?: "eager" | "lazy";
+}
+
+function MenuItem({ item, loading }: MenuItemProps) {
+    if (item.children.length > 0) {
+
+        return (
+            <>
+                <Drawer
+                    id={item.name}
+                    aside={
+                        <Drawer.Aside title="Menu" drawer={item.name}>
+                            <div
+                                class="flex flex-col h-full overflow-y-auto"
+                                style={{ minWidth: "100vw" }}
+                            >
+                                <ul class="px-4 flex-grow flex flex-col divide-y divide-base-200 overflow-y-auto">
+                                    {item.children.map((leaf) => (
+                                    <div class="collapse collapse-plus">
+                                         <div class="collapse-title">{leaf.name}</div>
+                                     </div>
+        
+                                    ))}
+                                </ul>
+                            </div>
+                        </Drawer.Aside>
+                    }
+                />
+
+                <label
+                    for={item.name}
+                    aria-label="open menu"
+                >
+                    <div class="collapse collapse-plus">
+                        <div class="collapse-title">{item.name}</div>
+                    </div>
+                </label>
+            </>
+        )
+
+    }
+
+    return (
+        <div class="collapse collapse-plus">
+            <div class="collapse-title">{item.name}</div>
+        </div>
+    )
+
+}
+
+function MenuMobile({ navItems = [] }: Props) {
+
+    console.log(navItems)
+
+    return (
+        <div
+            class="flex flex-col h-full overflow-y-auto"
+            style={{ minWidth: "100vw" }}
+        >
+            <ul class="px-4 flex-grow flex flex-col divide-y divide-base-200 overflow-y-auto">
+                {navItems.map((item) => (
+                    <li>
+                        <MenuItem item={item} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default MenuMobile;
---
+++ components/utils/device.ts
@@ -0,0 +1,12 @@
+// import { useScript } from "deco";
+
+// export const useDeviceType = () => {
+//   useScript(() => {
+//     const isMobile = window.innerWidth <= 1025;
+//     return isMobile ? "mobile" : "desktop";
+//   }, []);
+
+//   return typeof window !== "undefined" && window.innerWidth <= 1025
+//     ? "mobile"
+//     : "desktop";
+// };
