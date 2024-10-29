97dfa5e5919479cd61cdaaf8eda62f5b2cb84638
georgetonietti <georgetonietti@gmail.com>
2024-10-29 16:09:49 -0300

    feat: categories

--- sections/Category/CategoryGrid.tsx
+++ sections/Category/CategoryGrid.tsx
@@ -1,5 +1,3 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
-import Image from "apps/website/components/Image.tsx";
 import Section, {
   type Props as SectionHeaderProps,
 } from "../../components/ui/Section.tsx";
@@ -7,40 +5,46 @@ import Slider from "../../components/ui/Slider.tsx";
 import { clx } from "../../sdk/clx.ts";
 import { useDevice } from "@deco/deco/hooks";
 import { type LoadingFallbackProps } from "@deco/deco";
+import Icon from "../../components/ui/Icon.tsx";
+
 /** @titleBy label */
 export interface Item {
-  image: ImageWidget;
   href: string;
   label: string;
+  icon: string;
 }
 export interface Props extends SectionHeaderProps {
+  title: string;
   items: Item[];
 }
-function Card({ image, href, label }: Item) {
+function Card({ icon, href, label }: Item) {
   return (
-    <a href={href} class="flex flex-col items-center justify-center gap-4">
-      <div class="w-44 h-44 rounded-full bg-base-200 flex justify-center items-center border border-transparent hover:border-primary">
-        <Image
-          src={image}
-          alt={label}
-          width={100}
-          height={100}
-          loading="lazy"
-        />
+    <a href={href} class="flex flex-col items-center justify-center gap-4 max-w-[134px] mobile:w-[100px]">
+      <div class={clx(
+        "w-[120px] h-[120px] rounded-full bg-neutral-100 flex justify-center items-center",
+        "mobile:w-[90px] mobile:h-[90px]",
+        "hover:bg-info"
+        )}>
+        <Icon
+              id={icon}
+              width={57}
+              height={57}
+              class="object-contain w-full h-auto max-w-[57px] max-h-[57px]"
+            />
       </div>
-      <span class="font-medium text-sm">{label}</span>
+      <span class="font-bold text-black text-center text-sm w-full">{label}</span>
     </a>
   );
 }
-function CategoryGrid({ title, cta, items }: Props) {
+function CategoryGrid({ title, items }: Props) {
   const device = useDevice();
   return (
     <Section.Container>
-      <Section.Header title={title} cta={cta} />
+      <Section.Header title={title} />
 
       {device === "desktop"
         ? (
-          <div class="grid grid-cols-6 gap-10">
+          <div class="flex flex-nowrap justify-between w-full">
             {items.map((i) => <Card {...i} />)}
           </div>
         )
@@ -51,8 +55,8 @@ function CategoryGrid({ title, cta, items }: Props) {
                 index={index}
                 class={clx(
                   "carousel-item",
-                  "first:pl-5 first:sm:pl-0",
-                  "last:pr-5 last:sm:pr-0",
+                  "first:pl-5 first:mobile:pl-0",
+                  "last:pr-5 last:mobile:pr-0",
                 )}
               >
                 <Card {...i} />
@@ -64,10 +68,10 @@ function CategoryGrid({ title, cta, items }: Props) {
   );
 }
 export const LoadingFallback = (
-  { title, cta }: LoadingFallbackProps<Props>,
+  { title }: LoadingFallbackProps<Props>,
 ) => (
   <Section.Container>
-    <Section.Header title={title} cta={cta} />
+    <Section.Header title={title} />
     <Section.Placeholder height="212px" />;
   </Section.Container>
 );
