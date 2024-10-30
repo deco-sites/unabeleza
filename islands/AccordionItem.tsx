550b72a853db676dca39c89a54665879f10c29ff
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-30 15:38:38 -0300

    feat: fix mobile e responsive duvidas frequentes

--- islands/AccordionItem.tsx
+++ islands/AccordionItem.tsx
@@ -25,14 +25,23 @@ export default function AccordionItem({
         onClick={toggleAccordion}
       >
         <div className="flex items-center gap-2">
-          <span className="font-[Montserrat] flex items-center justify-center bg-[#A3E3FF] text-[#162116] text-[14px] leading-[18px] font-bold rounded-full w-[30px] h-[30px]">{questionNumber}</span>
-          <span className="font-[Montserrat] font-bold text-[14px] leading-[21px] text-[#162116]">{question}</span>
+          <span className="font-[Montserrat] flex items-center justify-center bg-[#A3E3FF] text-[#162116] text-[14px] font-bold rounded-full leading-none min-w-[30px] min-h-[30px]">
+            {questionNumber}
+          </span>
+          <span className="font-[Montserrat] font-bold text-[14px] leading-[21px] text-[#162116] mobile:font-medium mobile:text-left">
+            {question}
+          </span>
         </div>
-        <span className="ml-auto font-[Montserrat] bg-[#A3E3FF] text-[#162116] text-[20px] leading-[18px] font-bold rounded-full w-[30px] h-[30px] flex items-center justify-center">{isOpen ? "-" : "+"}</span>
+        <span className="ml-auto font-[Montserrat] bg-[#A3E3FF] text-[#162116] text-[20px] font-bold rounded-full flex items-center justify-center leading-none min-w-[30px] min-h-[30px]">
+          {isOpen ? "-" : "+"}
+        </span>
       </button>
       {isOpen && (
         <div className="mt-[23px]">
-          <p className="font-[Montserrat] font-medium text-[14px] leading-[21px] text-[#162116]" dangerouslySetInnerHTML={{ __html: anwser }} />
+          <p
+            className="font-[Montserrat] font-medium text-[14px] leading-[21px] text-[#162116] mobile:ml-[35px]"
+            dangerouslySetInnerHTML={{ __html: anwser }}
+          />
         </div>
       )}
     </div>
