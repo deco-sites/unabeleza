3b4aa2b4ed3cd85d63b6cc0dacbadf23ec53a9be
georgetonietti <georgetonietti@gmail.com>
2024-10-31 18:10:31 -0300

    feat: integration bug resolved

--- loaders/user.ts
+++ loaders/user.ts
@@ -3,6 +3,7 @@ import { AppContext } from "../apps/site.ts";
 import { usePlatform } from "../sdk/usePlatform.tsx";
 
 import { AppContext as AppContextVTEX } from "apps/vtex/mod.ts";
+import { AppContext as AppContextWAKE } from "apps/wake/mod.ts";
 
 async function loader(
   _: unknown,
@@ -18,6 +19,12 @@ async function loader(
   }
   if (platform === "shopify") {
     return null;
+  }
+
+  if (platform === "wake") {
+    const wake = ctx as unknown as AppContextWAKE;
+
+    return await wake.invoke("wake/loaders/user.ts");
   }
 
   throw new Error(`Unsupported platform: ${platform}`);
